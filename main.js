6e2ba2074441e6281a3304c1d6e8aafc62e9c91b
Carlos Peña <corochena@gmail.com>
2019-09-19 08:29:27 -0600

    Comma added to points

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ function rndPoints(width, height, n) {
   for (var i = 1; i < n; i++) {
     pList.push([
       //(2 * Math.random() * width) / n + pList[i - 1][0],
-      width * Math.random()
+      width * Math.random(),
       height * Math.random()
     ]);
   }
